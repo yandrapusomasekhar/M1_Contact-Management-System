63ad4a3f4c1dae9787958ea85d5f4d3ea9ee21e6
yandrapusomasekhar <101333790+yandrapusomasekhar@users.noreply.github.com>
2022-04-02 13:24:04 +0530

    Update main.c

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -6,18 +6,18 @@ struct employee {
     float empsalary;
 };
     struct employee e[100];
-void display();
-void create();
-void update();
-void search();
-void department();
-void name();
+void display();                                                         // This function is used for display the employees details
+void create();                                                          // This function is used for create new employee details
+void update();                                                          // This function is used for upadate the existing employee details
+void search();                                                          // This function is used for serach the employees in the programs
+void department();                                                      // This function is used for the to serach the employee with the help of department name
+void name();                                                            // This function is used to saerch the employee with help of name
 int l=10;
-void main() 
+void main()                                                             // This is the main function
 {
     int i,n;
-    int choice;
-    e[0]=(struct employee){1,100,"Ram","CSE",5000};
+    int choice;                                                         // to enter the choice for the swich startment
+    e[0]=(struct employee){1,100,"Ram","CSE",5000};// employee dateils like employee id , employee adhaar number ,employee name, employee department, employee salary
     e[1]=(struct employee){2,101,"Pavan","ECE",6000};
     e[2]=(struct employee){3,102,"Kevin","MECH",5500};
     e[3]=(struct employee){4,103,"Charan","CIVIL",4000};
@@ -37,7 +37,7 @@ void main()
     e[17]=(struct employee){18,117,"Prem","CST",9000};  
     e[18]=(struct employee){19,118,"Pavan","ECT",8000};
     e[19]=(struct employee){20,119,"John","MECH",9000};
-    printf("\t\t*WELCOME TO EMPLOYEE MANAGEMENT SYSTEM*\t\t\n");
+    printf("\t\t*WELCOME TO EMPLOYEE MANAGEMENT SYSTEM*\t\t\n");                            // To print the welcome massage on display
     while(1) {
         printf("\n\t\t**MAIN MENU*\t\t\n");
         printf("\t\t[1].Create details of an employee\n");
@@ -49,7 +49,7 @@ void main()
         printf("\t\t[7].Exit\n");
     printf("Enter your choice : ");
     scanf("%d",&choice);
-    switch(choice) {
+    switch(choice) {                                  // this switch statement is used for the folllowing functions like create, display, update, search, department, name
     case 1 : create();
             break;
     case 2 :display();
@@ -67,78 +67,79 @@ void main()
         
     }
 }
-void create() {
+void create() {                                                          // this create function should create the new employee details in the program
     int n;
  printf("Enter no.of employees in a company : ");
-            scanf("%d",&n);
+            scanf("%d",&n);                                             // to read the number of employees in the company
 
-            for(int i=0;i<n;i++) {
+            for(int i=0;i<n;i++) {                                      // the for loop is  used to iterate the statements or a part of the program several times
                 printf("Enter empid,empadhar,empname,empdept,empsalary : ");
                 scanf("%d%d\n%s%s%f",&e[l].empid,&e[l].empadhar,e[l].empname,e[l].empdept,&e[l].empsalary);
                 l++;
-                printf("\n");
+                printf("\n");                                               // print new line
             }
 }
-void display() {
+void display() {                                                            // this function to display the entire employees in the company
             int epid;
         //printf("Enter employee id number : ");
-       // scanf("%d",&epid);
+       // scanf("%d",&epid); // to read the employee id number
        printf(" \tEmpid\tEmpaddhar\tName\t\tDepartment\t\tSalary\n");
-        for(int i=0;i<l;i++) {
+        for(int i=0;i<l;i++) {                                              // the for loop is used to iterate the statements or a part of the program several times
             printf("\t%d\t%d\t\t%s\t\t\t%s\t\t%f\n",e[i].empid,e[i].empadhar,e[i].empname,e[i].empdept,e[i].empsalary);
             }
         }
     
-void update() {
+void update() {                                                             // This function is used to update the existing employee details
             int epid,n;
             float epsalary;
             printf("Enter employee id number : ");
-            scanf("%d",&epid);
+            scanf("%d",&epid);                                               // read the employee id number
             printf("Enter new salary : ");
-            scanf("%f",&epsalary);
+            scanf("%f",&epsalary);                                          // read the employee new salary
             //printf("id\tadhar\tname\tsalary\n");
-            for(int i=0;i<100;i++) {
-                if(epid==e[i].empid) {
-                    e[i].empsalary=epsalary;
+            for(int i=0;i<100;i++) {                                        //for loop is used to iterate the statements or a part of the program several times
+                if(epid==e[i].empid) {                                      // The if conditions is true whenever entered  employee id is equal to exsting employee id number
+                    e[i].empsalary=epsalary;                                // the new salary is replaced by the existing salary
                 }
-                //printf("%d\t%d\t%s\t%f\n",e[i].empid,e[i].empadhar,e[i].empname,e[i].empsalary);
+                //printf("%d\t%d\t%s\t%f\n",e[i].empid,e[i].empadhar,e[i].empname,e[i].empsalary); // print updated employee details with new salary
             }
     }
-void search() {
+void search() {                                                              // this function is used to search the employees in company
             int epid,n;
             printf("Enter employee id number : ");
-            scanf("%d",&epid);
+            scanf("%d",&epid);                                              // reda the employee id
             printf("\tiId\tAdhar\t\tName\t\tDepartment\t\tSalary\n");
-            for(int i=0;i<100;i++) {
-                if(epid==e[i].empid) {
+            for(int i=0;i<100;i++) {                                        //the for loop is used to iterate the statements or a part of the program several times
+                if(epid==e[i].empid) {                                      // The if conditions is true whenever entered  employee id is equal to exsting employee id number
                     printf("\t%d\t%d\t\t%s\t\t\t%s\t\t%f\n",e[i].empid,e[i].empadhar,e[i].empname,e[i].empdept,e[i].empsalary);
                 }
             }
   }  
- void department() {
+ void department() {                                                        // This function is used to serach the employee for perticular department
     char epdp[20];
     int f=0;
     printf("Enter employees department : ");
-    scanf("%s",epdp);
+    scanf("%s",epdp);                                                       // read the department name
     printf("\tId\tAdhar\t\tName\t\tDepartment\t\tSalary\n");
-    for(int i=0;i<100;i++) {
-        if(strcmp(epdp,e[i].empdept)==0) {
+    for(int i=0;i<100;i++) {                                                // the for loop is used to iterate the statements or a part of the program several times
+        if(strcmp(epdp,e[i].empdept)==0) {                                  // the if condition  is compare the two strings
             printf("\t%d\t%d\t\t%s\t\t\t%s\t\t%f\n",e[i].empid,e[i].empadhar,e[i].empname,e[i].empdept,e[i].empsalary);
             f=1;
         }
     }
-    if(f==0) {
-        printf("Enter valid depatment\n");
+    if(f==0) {                                                              // if enterd department is not there is the employee details
+        printf("Enter valid depatment\n");  
     }
  }
  
- void name() {
+ void name() {                                                              // This function is used to find the employee in the company by using name
          char epname[100],n;
             printf("Enter employee name : ");
-            scanf("%s",epname);
+            scanf("%s",epname);                                               // read the employee name
             printf("\tiId\tAdhar\t\tName\t\tDepartment\t\tSalary\n");
-            for(int i=0;i<100;i++) {
-                if(strcmp(epname,e[i].empname)==0) {
+            for(int i=0;i<100;i++) {                                          // the for loop is used to iterate the statements or a part of the program several times
+                if(strcmp(epname,e[i].empname)==0)                            // compare the employee names
+                {
                     printf("\t%d\t%d\t\t%s\t\t\t%s\t\t%f\n",e[i].empid,e[i].empadhar,e[i].empname,e[i].empdept,e[i].empsalary);
                 }
                 
